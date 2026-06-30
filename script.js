7ca74beb3bab33e7b31e14261b7be7ef55b3a05d
rorawan791-code <rorawan791@gmail.com>
2026-06-30 22:04:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-document.querySelector("button").onclick = function () {
-    alert("Welcome to Our Little Universe ❤️");
+window.onload = function () {
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", function(){
+
+button.innerHTML = "❤️ Welcome Home ❤️";
+
+button.style.background = "#ff4f8b";
+button.style.color = "white";
+
+document.body.style.transition = "2s";
+document.body.style.background =
+"linear-gradient(180deg,#030712,#0f1f52,#29124d)";
+
+});
+
 };
