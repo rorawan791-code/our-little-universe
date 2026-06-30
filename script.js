8bd6fed7b2bbcb0511e20a55c323839d5a9104f7
rorawan791-code <rorawan791@gmail.com>
2026-07-01 00:36:48 +0300

    Update Add love counters

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ document.body.style.background =
 });
 
 };
+// ===== Love Counters =====
+
+const engagedDate = new Date("2025-01-24");
+const weddingDate = new Date("2026-08-28");
+
+function updateCounters() {
+    const today = new Date();
+
+    const engagedDays = Math.floor((today - engagedDate) / (1000 * 60 * 60 * 24));
+    const weddingDays = Math.floor((weddingDate - today) / (1000 * 60 * 60 * 24));
+
+    const cards = document.querySelectorAll(".time-card");
+
+    if (cards.length >= 2) {
+        cards[0].querySelector("p").innerHTML =
+            "24 January 2025<br><br>" +
+            engagedDays + " days together ❤️";
+
+        cards[1].querySelector("p").innerHTML =
+            "28 August 2026<br><br>" +
+            weddingDays + " days left 💍";
+    }
+}
+
+updateCounters();
