c19d62a208717817ab95163c70bd6625c3ca2d4c
rorawan791-code <rorawan791@gmail.com>
2026-07-01 04:54:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -75,14 +75,42 @@ const letterText = document.getElementById("letterText");
 
 hiddenStar.onclick = function () {
 
-    letterText.innerHTML = "";
+    letterText.textContent = "";
+    letterText.style.whiteSpace = "pre-line";
     secretLetter.style.display = "flex";
 
-    const text = `We were never just a story...
-we were a universe written in stars.
+    const text = `هاي زوجي المستقبلي و حبيب عيوني
 
-I love you baby.
-Can't wait to be yours...
+بكتبلك الكلام ده وأنا عارفة إننا الاتنين مطحونين، وبقالنا فترة كلامنا ومكالماتنا كلها عبارة عن (جبت إيه؟، ناقص إيه؟، المشوار الفلاني هنعمل فيه إيه؟)
+
+أنا عارفة ومقدرة التعب والضغط اللي أنت فيه الفترة دي وشايفة كل مجهود بتعمله عشان بيتنا يطلع حلو وعشان نكون سوا. أنا بجد فخورة بيك وبمشاويرك وتعبك، وممتنة لكل خطوة بتتعب فيها علشاني وعلشان مستقبلنا، حتى لو بسبب الضغط مش بعرف أعبر عن ده.
+
+عارف؟ في وسط خناقاتنا وااختلافاتنا الصغيرة بتاعة التجهيزات، بفتكر إن شهرين وكل الدوشة دي تخلص والباب يتقفل علينا...
+
+and it's Worth it ❤️
+
+أنا مش عايزة ضغط الأيام دي ينسينا إحنا بنعمل كل ده ليه.
+
+إحنا بنعمل ده عشان بنحب بعض، وعشان مفيش حد في الدنيا دي كلها ينفع أكون معاه غيرك أنت.
+
+وعشان من البداية قولت إن كل الأشخاص فيهم عيوب، وكل العلاقات فيها مشاكل، لكن أنت الشخص الوحيد اللي مستعدة أخوض ده معاه.
+
+وتذكير سريع...
+
+إني بحبك وإحنا كويسين.
+وبحبك وإحنا متخانقين.
+وبحبك كامل.
+وبحب عيوبك.
+وبحبك وإحنا ساكتين، زي ما بحبك وإحنا بنتكلم 24/7.
+
+وبحبك دلوقتي...
+وهحبك أكتر.
+
+❤️
+
+I love you baby,
+can't wait to be yours...
+
 Your woman 🫂`;
 
     clearInterval(window.typingInterval);
@@ -91,7 +119,7 @@ Your woman 🫂`;
 
     window.typingInterval = setInterval(function(){
 
-        letterText.innerHTML += text.charAt(i);
+        letterText.textContent += text.charAt(i);
 
         i++;
 
@@ -99,7 +127,7 @@ Your woman 🫂`;
             clearInterval(window.typingInterval);
         }
 
-    },40);
+    }, 40);
 
 };
 
