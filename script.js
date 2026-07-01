a965526b9326a94b4a431a907665384904d48e0f
rorawan791-code <rorawan791@gmail.com>
2026-07-01 08:40:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -66,12 +66,74 @@ const textBox = document.getElementById("letterText");
 const closeLetter = document.getElementById("closeLetter");
 
 star.onclick = ()=>{
+
     popup.style.display="flex";
-    let text = "I love you baby ❤️";
-    textBox.textContent = text;
+
+    textBox.textContent = "";
+    textBox.style.whiteSpace = "pre-line";
+
+    const text = `هاي زوجي المستقبلي و حبيب عيوني ❤️
+
+بكتبلك الكلام ده وأنا عارفة إننا الاتنين مطحونين، وبقالنا فترة كلامنا ومكالماتنا كلها عبارة عن (جبت إيه؟، ناقص إيه؟، المشوار الفلاني هنعمل فيه إيه؟)
+
+أنا عارفة ومقدرة التعب والضغط اللي أنت فيه الفترة دي وشايفة كل مجهود بتعمله عشان بيتنا يطلع حلو وعشان نكون سوا. أنا بجد فخورة بيك وبمشاويرك وتعبك وممتنة لكل خطوة بتتعب فيها علشاني وعلشان مستقبلنا حتى لو بسبب الضغط مش بعرف أعبر عن ده.
+
+عارف؟ في وسط خناقاتنا وااختلافاتنا الصغيرة بتاعة التجهيزات، بفتكر إن شهرين وكل الدوشة دي تخلص والباب يتقفل علينا...
+
+and it's Worth it ❤️
+
+أنا مش عايزة ضغط الأيام دي ينسينا إحنا بنعمل كل ده ليه.
+
+إحنا بنعمل ده عشان بنحب بعض، وعشان مفيش حد في الدنيا دي كلها ينفع أكون معاه غيرك أنت.
+
+وعشان من البداية قولت إن كل الأشخاص فيهم عيوب، وكل العلاقات فيها مشاكل، لكن أنت الشخص الوحيد اللي مستعدة أخوض ده معاه.
+
+وتذكير سريع...
+
+إني بحبك وإحنا كويسين.
+وبحبك وإحنا متخانقين.
+وبحبك كامل.
+وبحب عيوبك.
+وبحبك وإحنا ساكتين، زي ما بحبك وإحنا بنتكلم 24/7.
+
+وبحبك دلوقتي...
+وهحبك أكتر.
+
+❤️
+
+I love you baby,
+can't wait to be yours...
+
+Your woman 🫂`;
+
+    clearInterval(window.typingInterval);
+
+    let index = 0;
+
+    window.typingInterval = setInterval(()=>{
+
+        textBox.textContent += text.charAt(index);
+        index++;
+
+        if(index >= text.length){
+            clearInterval(window.typingInterval);
+        }
+
+    },40);
+
 };
 
-closeLetter.onclick = ()=>popup.style.display="none";
+closeLetter.onclick = ()=>{
+    clearInterval(window.typingInterval);
+    popup.style.display="none";
+};
+
+popup.onclick = (event)=>{
+    if(event.target===popup){
+        clearInterval(window.typingInterval);
+        popup.style.display="none";
+    }
+};
 
 // ===== RUSTICA =====
 const rusticaBtn = document.getElementById("rusticaBtn");
